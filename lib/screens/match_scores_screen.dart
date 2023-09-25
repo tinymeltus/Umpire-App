5ce597d2fa11205d4df32dc9faf9662c8ae896ee
tiny meltus <eogolla5@gmail.com>
2023-09-25 11:29:20 +0300

    updated match screen

--- lib/screens/match_scores_screen.dart
+++ lib/screens/match_scores_screen.dart
@@ -33,22 +33,19 @@ class _MatchScoresScreenState extends State<MatchScoresScreen> {
   //gameScores
   int scorePlayerA = 0;
   int scorePlayerB = 0;
-
-  int gamesPlayerA = 0; // Declare setsWonPlayerA
-  int gamesPlayerB = 0; // Declare setsWonPlayerB
-
-  int gamesWonPlayerA = 0;
-  int gamesWonPlayerB = 0;
-
-  bool advantagePlayerA = false;
-  bool advantagePlayerB = false;
-
+  int gamesPlayerA = 0;
+  int gamesPlayerB = 0;
   int setsWonPlayerA = 0;
   int setsWonPlayerB = 0;
+  int numberOfSetsToWinMatch = 3; // TODO: change to dynamic value
+  String matchWinner = '';
+  bool isDeuce = false;
 
-  // Add these two variables to track games won per set
+  // Variables for tracking games won per set
   int gamesWonByPlayerAInSet = 0;
   int gamesWonByPlayerBInSet = 0;
+
+  List<List<int>> setScores = [];
 
   GameTimer _gameTimer = GameTimer();
 
@@ -94,6 +91,18 @@ class _MatchScoresScreenState extends State<MatchScoresScreen> {
               ),
 
               // Other widgets can be added here...
+              Expanded(
+                child: ListView.builder(
+                  itemCount: setScores.length,
+                  itemBuilder: (context, index) {
+                    final setScore = setScores[index];
+                    final setNumber = index + 1;
+
+                    return Text(
+                        'Set $setNumber: ${setScore[0]} - ${setScore[1]}');
+                  },
+                ),
+              )
             ],
           ),
 
@@ -105,12 +114,14 @@ class _MatchScoresScreenState extends State<MatchScoresScreen> {
                 ElevatedButton(
                   onPressed: () {
                     _incrementScore('A');
+                    print(scorePlayerA);
                   },
                   child: Text('Point A'),
                 ),
                 ElevatedButton(
                   onPressed: () {
                     _incrementScore('B');
+                    print(scorePlayerB);
                   },
                   child: Text('Point B'),
                 ),
@@ -179,111 +190,93 @@ class _MatchScoresScreenState extends State<MatchScoresScreen> {
   //methods
   //count game scores
   //scoring rules with duece situation included
-  void _incrementScore(String player) {
-    final lastChange = ScoreChangeEvent(scorePlayerA, scorePlayerB);
-    widget.scoreChangeHistory.add(lastChange);
-    if (player == 'A') {
-      if (scorePlayerA == 0) {
-        scorePlayerA = 15;
-      } else if (scorePlayerA == 15) {
-        scorePlayerA = 30;
-      } else if (scorePlayerA == 30) {
-        scorePlayerA = 40;
-      } else if (scorePlayerA == 40) {
-        if (scorePlayerB == 40) {
-          // Scores are tied at "deuce," increment by "Advantage" for Player A
-          scorePlayerA = 45; // "45" means "Advantage" for Player A
-          scorePlayerB = 40; // Reset Player B's score from 40 to 40
-        } else if (scorePlayerB == 45) {
-          // Player B had "Advantage," scores are back to "deuce"
-          scorePlayerA = 40;
-          scorePlayerB = 40;
-        } else {
-          // Player A wins the game
-          gamesPlayerA++; // Increment game count for Player A
-          scorePlayerA = 0; // Reset game score for Player A
-          scorePlayerB = 0; // Reset game score for Player B
-          // Check if a player has won the set
-          if (gamesWonByPlayerAInSet ==
-              widget.matchDetails.numberOfGamesPerSet) {
-            setsWonPlayerA++; // Increment sets won by Player A
-            gamesWonByPlayerAInSet =
-                0; // Reset games won by Player A in the set
-            gamesWonByPlayerBInSet =
-                0; // Reset games won by Player B in the set
-          }
-        }
-      } else if (scorePlayerA == 45) {
-        // Player A has "Advantage"
-        scorePlayerA = 50; // "50" means "Game Point" for Player A
-      } else if (scorePlayerA == 50) {
-        // Player A wins the game
-        gamesPlayerA++; // Increment game count for Player A
-        scorePlayerA = 0; // Reset game score for Player A
-        scorePlayerB = 0; // Reset game score for Player B
-        // Check if a player has won the set
-        if (gamesWonByPlayerAInSet == widget.matchDetails.numberOfGamesPerSet) {
-          setsWonPlayerA++; // Increment sets won by Player A
-          gamesWonByPlayerAInSet = 0; // Reset games won by Player A in the set
-          gamesWonByPlayerBInSet = 0; // Reset games won by Player B in the set
-        }
-      } else {
-        // Handle other cases or show an error message
-      }
-    } else if (player == 'B') {
-      final lastChange = ScoreChangeEvent(scorePlayerA, scorePlayerB);
-      widget.scoreChangeHistory.add(lastChange);
-      if (scorePlayerB == 0) {
-        scorePlayerB = 15;
-      } else if (scorePlayerB == 15) {
-        scorePlayerB = 30;
-      } else if (scorePlayerB == 30) {
-        scorePlayerB = 40;
-      } else if (scorePlayerB == 40) {
-        if (scorePlayerA == 40) {
-          // Scores are tied at "deuce," increment by "Advantage" for Player B
-          scorePlayerB = 45; // "45" means "Advantage" for Player B
-          scorePlayerA = 40; // Reset Player A's score from 40 to 40
-        } else if (scorePlayerA == 45) {
-          // Player A had "Advantage," scores are back to "deuce"
-          scorePlayerA = 40;
-          scorePlayerB = 40;
-        } else {
-          // Player B wins the game
-          gamesPlayerB++; // Increment game count for Player B
-          scorePlayerA = 0; // Reset game score for Player A
-          scorePlayerB = 0; // Reset game score for Player B
-          // Check if a player has won the set
-          if (gamesWonByPlayerBInSet ==
-              widget.matchDetails.numberOfGamesPerSet) {
-            setsWonPlayerB++; // Increment sets won by Player A
-            gamesWonByPlayerAInSet =
-                0; // Reset games won by Player A in the set
-            gamesWonByPlayerBInSet =
-                0; // Reset games won by Player B in the set
-          }
-        }
-      } else if (scorePlayerB == 45) {
-        // Player B has "Advantage"
-        scorePlayerB = 50; // "50" means "Game Point" for Player B
-      } else if (scorePlayerB == 50) {
-        // Player B wins the game
-        gamesPlayerB++; // Increment game count for Player B
-        scorePlayerA = 0; // Reset game score for Player A
-        scorePlayerB = 0; // Reset game score for Player B
-        // Check if a player has won the set
-        if (gamesWonByPlayerAInSet == widget.matchDetails.numberOfGamesPerSet) {
-          setsWonPlayerA++; // Increment sets won by Player A
-          gamesWonByPlayerAInSet = 0; // Reset games won by Player A in the set
-          gamesWonByPlayerBInSet = 0; // Reset games won by Player B in the set
-        }
-      } else {
-        // Handle other cases or show an error message
-      }
-    }
+ void _incrementScore(String player) {
+  final lastChange = ScoreChangeEvent(scorePlayerA, scorePlayerB);
+  widget.scoreChangeHistory.add(lastChange);
 
-    setState(() {});
+  if (player == 'A') {
+    _handleScoringForPlayerA();
+  } else if (player == 'B') {
+    _handleScoringForPlayerB();
   }
+
+  setState(() {});
+}
+
+void _handleScoringForPlayerA() {
+  if (scorePlayerA == 0) {
+    scorePlayerA = 15;
+  } else if (scorePlayerA == 15) {
+    scorePlayerA = 30;
+  } else if (scorePlayerA == 30) {
+    scorePlayerA = 40;
+  } else if (scorePlayerA == 40) {
+    if (scorePlayerB == 40) {
+      // Scores are tied at "deuce," increment by "Advantage" for Player A
+      scorePlayerA = 45; // "45" means "Advantage" for Player A
+      scorePlayerB = 40; // Reset Player B's score from 40 to 40
+      isDeuce = true;
+    } else if (scorePlayerB == 45) {
+      // Player B had "Advantage," scores are back to "deuce"
+      scorePlayerA = 40;
+      scorePlayerB = 40;
+      isDeuce = true;
+    } else {
+      // Player A wins the game
+      gamesPlayerA++; // Increment game count for Player A
+      scorePlayerA = 0; // Reset game score for Player A
+      scorePlayerB = 0; // Reset game score for Player B
+    }
+  } else if (scorePlayerA == 45) {
+    // Player A has "Advantage"
+    scorePlayerA = 50; // "50" means "Game Point" for Player A
+  } else if (scorePlayerA == 50) {
+    // Player A wins the game
+    gamesPlayerA++; // Increment game count for Player A
+    scorePlayerA = 0; // Reset game score for Player A
+    scorePlayerB = 0; // Reset game score for Player B
+  } else {
+    // Handle other cases or show an error message
+  }
+}
+
+void _handleScoringForPlayerB() {
+  if (scorePlayerB == 0) {
+    scorePlayerB = 15;
+  } else if (scorePlayerB == 15) {
+    scorePlayerB = 30;
+  } else if (scorePlayerB == 30) {
+    scorePlayerB = 40;
+  } else if (scorePlayerB == 40) {
+    if (scorePlayerA == 40) {
+      // Scores are tied at "deuce," increment by "Advantage" for Player B
+      scorePlayerB = 45; // "45" means "Advantage" for Player B
+      scorePlayerA = 40; // Reset Player A's score from 40 to 40
+      isDeuce = true;
+    } else if (scorePlayerA == 45) {
+      // Player A had "Advantage," scores are back to "deuce"
+      scorePlayerA = 40;
+      scorePlayerB = 40;
+      isDeuce = true;
+    } else {
+      // Player B wins the game
+      gamesPlayerB++; // Increment game count for Player B
+      scorePlayerA = 0; // Reset game score for Player A
+      scorePlayerB = 0; // Reset game score for Player B
+    }
+  } else if (scorePlayerB == 45) {
+    // Player B has "Advantage"
+    scorePlayerB = 50; // "50" means "Game Point" for Player B
+  } else if (scorePlayerB == 50) {
+    // Player B wins the game
+    gamesPlayerB++; // Increment game count for Player B
+    scorePlayerA = 0; // Reset game score for Player A
+    scorePlayerB = 0; // Reset game score for Player B
+  } else {
+    // Handle other cases or show an error message
+  }
+}
+
 
   //
   String getTennisScore(int score) {
@@ -301,6 +294,81 @@ class _MatchScoresScreenState extends State<MatchScoresScreen> {
       return "Game";
     } else {
       return "Unknown";
+    }
+  }
+
+  // Check for game winner and update gamesPlayerA or gamesPlayerB
+// Reset game scores to deuce (40-40) when applicable
+  void _checkGameWinner() {
+    if (scorePlayerA == 40 && scorePlayerB == 40) {
+      // Both players are at 40-40, indicating a "deuce" situation
+      // Handle deuce logic here, for example:
+      // This code sets both players' scores back to 40-40
+      scorePlayerA = 40;
+      scorePlayerB = 40;
+    } else if (scorePlayerA == 45) {
+      // Player A has "Advantage"
+      // Handle advantage logic here, for example:
+      // Player A wins the game
+      gamesPlayerA++; // Increment game count for Player A
+      scorePlayerA = 0; // Reset game score for Player A
+      scorePlayerB = 0; // Reset game score for Player B
+    } else if (scorePlayerB == 45) {
+      // Player B has "Advantage"
+      // Handle advantage logic here, for example:
+      // Player B wins the game
+      gamesPlayerB++; // Increment game count for Player B
+      scorePlayerA = 0; // Reset game score for Player A
+      scorePlayerB = 0; // Reset game score for Player B
+    } else {
+      // Handle other game-winning conditions
+      // For example, if none of the above conditions are met,
+      // you can consider it a normal game win for one of the players
+      if (scorePlayerA == 50) {
+        // Player A wins the game
+        gamesPlayerA++; // Increment game count for Player A
+        scorePlayerA = 0; // Reset game score for Player A
+        scorePlayerB = 0; // Reset game score for Player B
+      } else if (scorePlayerB == 50) {
+        // Player B wins the game
+        gamesPlayerB++; // Increment game count for Player B
+        scorePlayerA = 0; // Reset game score for Player A
+        scorePlayerB = 0; // Reset game score for Player B
+      }
+    }
+  }
+
+  // Check for set winner and update setsPlayerA or setsPlayerB
+  // Reset game scores and gamesPlayerA/gamesPlayerB to deuce (40-40) when applicable
+  void _checkSetWinner() {
+    if (gamesPlayerA == widget.matchDetails.numberOfGamesPerSet ||
+        gamesPlayerB == widget.matchDetails.numberOfGamesPerSet) {
+      // Reset game scores to deuce (40-40)
+      scorePlayerA = 40;
+      scorePlayerB = 40;
+
+      // Add the scores to the setScores list
+      setScores.add([gamesPlayerA, gamesPlayerB]);
+
+      // Reset games won
+      gamesPlayerA = 0;
+      gamesPlayerB = 0;
+
+      // Increment sets won
+      if (gamesWonByPlayerAInSet > gamesWonByPlayerBInSet) {
+        setsWonPlayerA++;
+      } else {
+        setsWonPlayerB++;
+      }
+    }
+  }
+
+// Check for match winner
+  void _checkMatchWinner() {
+    if (setsWonPlayerA == numberOfSetsToWinMatch) {
+      matchWinner = 'A'; // Player A wins the match
+    } else if (setsWonPlayerB == numberOfSetsToWinMatch) {
+      matchWinner = 'B'; // Player B wins the match
     }
   }
 }
